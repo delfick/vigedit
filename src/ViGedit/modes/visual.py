0868e178c4839b150510d5c4ae8958ed6bb0eeae
Stephen Moore <stephen@delfick.com>
2008-06-19 09:34:10 +0800

    updated docs some more

--- src/ViGedit/modes/visual.py
+++ src/ViGedit/modes/visual.py
@@ -29,7 +29,6 @@ class visual_Mode(binding_base):
         self.register(insert.insert_begin_line, gtk.keysyms.I, True, False, "insert")
         self.register(lambda : wrap.preserve_mode(insert.open_line_below), gtk.keysyms.o, True, True)
         self.register(lambda : wrap.preserve_mode(insert.open_line_above), gtk.keysyms.O, True, True)
-        self.register(insert.append_after, gtk.keysyms.a, True, False, "insert")
 
         self.register(others.undo, gtk.keysyms.u, True, True)
         self.register(others.search, gtk.keysyms.slash, True)
