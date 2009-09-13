c9608dd91a951a79a2a531eef6abd9d65972a328
Stephen Moore <stephen@delfick.com>
2009-09-13 16:47:43 +0800

    fixed install.py

--- install.py
+++ install.py
@@ -1,30 +1,48 @@
 #!/usr/bin/env python
 
 import os
-from distutils.file_util import *
-from distutils.dir_util import *
+from distutils.file_util import copy_file
+from distutils.dir_util import copy_tree
 
-GEDIT_BASE = os.environ["HOME"] + "/.gnome2/gedit"
-TARGET = GEDIT_BASE + "/plugins"
+GEDIT_BASE = '%s/%s' %(os.environ["HOME"], '.gnome2/gedit')
+TARGET = '%s/plugins' % GEDIT_BASE
 
-if os.path.exists(TARGET) != True:
-    os.mkdir(GEDIT_BASE)
-    os.mkdir(TARGET)
+def makeFolder(target, desc):
+    """Create a folder and ignore errors if that folder already exists"""
+    try:
+        os.makedirs(target)
+        print "Made folder at %s" % target
+    except OSError:
+        if not os.path.exists(target):
+            raise Exception, "Failed to create %s at - %s" % (desc, target)
+        #Folder must already exist
 
-def install_plugin(name):
-    plugin_dir = TARGET + "/" + name 
-    copy_file("data/" + name.lower() + ".gedit-plugin", TARGET)
-    base_name = "src/" + name.lower()
-    single_file = base_name + "/" + name.lower() + ".py"
-    if os.path.exists(single_file):
-       copy_file(single_file, TARGET) 
+
+#Make sure there is a plugin folder for gedit
+makeFolder(TARGET, "plugin folder")
+
+def installPlugin(name):
+    #copy the .gedit-plugin file
+    copy_file("data/%s.gedit-plugin" % name.lower(), TARGET)
+    
+    base = "src/%s" % name
+    singleFile = "%s/%s.py" % (base, name.lower())
+    
+    if os.path.exists(singleFile):
+        #plugin is only a single file
+        #however that may not always be the case, but here it is, so whatever.....
+        copy_file(singleFile, TARGET) 
+        print "Copied %s to %s" % (singleFile, TARGET)
     else:
-        if os.path.exists(plugin_dir) != True:
-            os.mkdir(plugin_dir)
-        copy_tree("src/" + name, plugin_dir)      
+        #Create folder for this plugin
+        pluginDir = '%s/%s' % (TARGET, name)
+        makeFolder(pluginDir, "%s folder" % name)
+        copy_tree("src/%s" % name, pluginDir)      
+        print "Copied plugin to %s" % pluginDir
 
-install_plugin("ViGedit")
-install_plugin("autotab")
+installPlugin("ViGedit")
+installPlugin("autotab")
+
 # Considering integration with other plugins
-# install_plugin("classbrowser")
+# installPlugin("classbrowser")
 
