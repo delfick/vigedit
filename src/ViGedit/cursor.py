48d03a2a6097dd3886bc01c1582fe9b3e6734ed5
Stephen Moore <stephen@delfick.com>
2012-04-30 22:04:44 +1000

    Removing unnecessary licence crap from the top of files

--- src/ViGedit/cursor.py
+++ src/ViGedit/cursor.py
@@ -1,26 +1,3 @@
-# -*- coding: utf-8 -*-
-
-#  cursor.py - processing for the cursor
-#
-#  Copyright (C) 2008 - Joseph Method
-#  Copyright (C) 2008, 2009 - Stephen Moore
-#  Copyright (C) 2006 - Trond Danielsen
-#
-#  This program is free software; you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software
-#  Foundation, Inc., 59 Temple Place, Suite 330,
-#  Boston, MA 02111-1307, USA.
-
 import string
 token_symbols = string.letters + string.digits + '_'
 
